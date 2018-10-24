78af23dbc527139b52103d2fcb12c6280ffb292d
suwei-PC <su_wei1989@qq.com>
2018-10-24 16:58:10 +0800

    致设计移动端

---
+++ zhisheji-m/js/home.js
@@ -0,0 +1,1 @@
+$(function(){!function(){$(".showNav").click(function(){$(".cd-side-navigation").addClass("cd-side-on")}),$(".cd-side-navigation").click(function(){$(".cd-side-navigation").removeClass("cd-side-on")}),$(".cd-side-navigation ul").click(function(i){i.stopPropagation()})}()});
