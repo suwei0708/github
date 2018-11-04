f74d787ae7ccc99d1a1132d7203c8024f8cbf6e9
suwei-MPC <304900839@qq.com>
2018-11-05 01:29:30 +0800

    板块制作

---
+++ pindao/lvyou/js/page.js
@@ -0,0 +1,93 @@
+(function($) {
+$(function() {
+    // 轮播图banner
+    if($('#sld-banner').length) {
+        $('#sld-banner').slides({
+            generatePagination: true,
+            generateNextPrev: true,
+            play: 3000,
+            pause: 2500,
+            hoverPause: true,
+            effect: 'fade'
+        });
+    };
+    /*
+     * 旅游频道
+    */
+    // 旅游轮播图1
+    if($('#ly-sld1').length) {
+        $('#ly-sld1').slides({
+            generatePagination: false,
+            generateNextPrev: false,
+            play: 3000,
+            pause: 2500,
+            hoverPause: true
+        });
+    };
+
+    // 返回顶部
+    $('.ly-talk').on('click', '.back', function() {
+        $('body, html').animate({
+            scrollTop: 0
+        }, 200);
+        return false;
+    });
+
+    // 菜单悬浮
+    var navPos = $('.ly-nav').offset().top;
+    fixedNav(navPos);
+    $(window).on('scroll', function() {
+        fixedNav(navPos);
+    });
+
+    // 搜索下拉
+    $('.so-box').on('mouseover', '.select', function() {
+        $('.so-box').find('.select-box').show();
+    })
+    .on('mouseout', '.select', function() {
+        $('.so-box').find('.select-box').hide();
+    })
+    .on('click', '.select-box li', function() {
+        $('.so-box').find('.select').find('.txt').text($(this).text());
+        $('.so-box').find('.select-box').hide();
+    });
+
+    // 显示更多
+    $.each($('.ly-morepics'), function(index) {
+        var num = 5;
+        if($(this).parents('.ly-pics').hasClass('ly-tuwen')) {
+            num = 7
+        }
+        $(this).parents('.ly-pics').find('li:gt(' + num + ')').hide();
+    });
+
+    $('.ly-morepics').on('click', function() {
+        if($(this).text() == '显示更多') {
+            $(this).text('收起');
+            $(this).parents('.ly-pics').find('li').show();
+        }
+        else {
+            $(this).text('显示更多');
+            var num = 5;
+            if($(this).parents('.ly-pics').hasClass('ly-tuwen')) {
+                num = 7
+            }
+            $(this).parents('.ly-pics').find('li:gt(' + num + ')').hide();
+        }
+    });
+});
+
+function fixedNav(navPos) {
+    if($(window).scrollTop() > navPos) {
+        $('.ly-nav').css({
+            'position': 'fixed'
+        });
+    }
+    else {
+        $('.ly-nav').css({
+            'position': 'static'
+        });
+    }
+}
+
+})(jQuery);
