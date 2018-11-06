8dad948d917c0be109ab7bd84dc61bc2de381927
suwei-PC <su_wei1989@qq.com>
2018-11-06 17:40:19 +0800

    旅游频道修改、致设计视频居中，十强修改

--- 2018shiqiang/js/pagezt.js
+++ 2018shiqiang/js/pagezt.js
@@ -71,10 +71,93 @@ $(function() {
         });
     };
 
-    $.ztMsg.Alert('zan', 'msg', 'btntxt')
+    // 首页参选
+    $('.ztbtn-canxuan').on('click', function() {
+        var cxStatus = rnd(0, 1); //随机生成0，1
+        if(cxStatus == 0) {
+            // 参选成功
+            $.ztMsg.Alert('zan', '参与成功！开始为自己拉票吧~', '进入我的参赛主页', '个人拉票页.html#navlink');
+        }
+        else {
+            // 参选失败
+            $.ztMsg.Confirm('zan', '需要 3 个或以上原创作品才能参加哟~', '去上传', 'http://www.zhisheji.com/zuopin/product/add/2/0');
+        }
+    });
+
 });
 })(jQuery);
 
+// alert和confirm美化，调用方法
+// $.ztMsg.Alert('icon', 'msg', 'btntxt');
+// $.ztMsg.Confirm('icon', 'msg', 'btntxt', func);
+(function() {
+    jQuery.ztMsg = {
+        Alert: function(icon, msg, btntxt, btnlink) {
+            GenerateHtml('alert', icon, msg, btntxt, btnlink);
+            btnOk();
+            btnNo();
+        },
+        Confirm: function(icon, msg, btntxt, btnlink, callback) {
+            GenerateHtml('confirm', icon, msg, btntxt, btnlink);
+            btnOk(callback);
+            btnNo();
+        }
+    }
+    //生成Html
+    var GenerateHtml = function(type, icon, msg, btntxt, btnlink) {
+        var _html = '<div id="ztpopup" class="ztpopup ztpopup-' + icon + '"><div id="ztpopup-box" class="ztpopup-box"><a href="javascript:;" class="ztpopup-close" id="zt-close"><span class="icon-fail"></span></a>';
+        _html += '<div class="ztpopup-ct"><span class="tipicon tipicon-' + icon + '"></span><p>' + msg + '</p></div>';
+        if(!btnlink) {
+            btnlink = 'javascript:;'
+        }
+        if(!btntxt) {
+            btntxt = '确定'
+        }
+        else {
+            if (type == 'alert') {
+                _html += '<div class="ztpopup-btn"><a class="btn" id="zt-ok" href="' + btnlink + '">' + btntxt +'</a></div>';
+            }
+            else if (type == 'confirm') {
+                _html += '<div class="ztpopup-btn"><a class="btn-sure" id="zt-ok" href="' + btnlink + '">' + btntxt +'</a>';
+                _html += '<a class="btn-cancel" id="zt-no" href="javascript:;">再看看</a></div>';
+            }
+        }
+        _html += '</div></div>';
+        //必须先将_html添加到body，再设置Css样式
+        jQuery('body').append(_html);
+        GenerateCss();
+    }
+
+    //生成css
+    var GenerateCss = function() {
+        var _widht = document.documentElement.clientWidth; //屏幕宽
+        var _height = document.documentElement.clientHeight; //屏幕高
+        var boxWidth = jQuery('#ztpopup-box').outerWidth();
+        var boxHeight = jQuery('#ztpopup-box').outerHeight();
+        //让提示框居中
+        jQuery('#ztpopup-box').css({
+            top: (_height - boxHeight) / 2 + 'px',
+            left: (_widht - boxWidth) / 2 + 'px'
+        });
+    }
+    //确定按钮事件
+    var btnOk = function(callback) {
+        jQuery('#zt-ok').on('click', function() {
+            jQuery('#ztpopup').remove();
+            if (typeof(callback) == 'function') {
+                callback();
+            }
+        });
+    }
+    //取消按钮事件
+    var btnNo = function() {
+        jQuery('#zt-no, #zt-close').on('click', function() {
+            jQuery('#ztpopup').remove();
+        });
+    }
+})();
+
+// 悬浮滚动
 function showFixNav() {
     if($(window).scrollTop() > $('#scroll0').offset().top - 82) {
         $('.fixed-nav').show();
@@ -152,67 +235,3 @@ function copyToClipboard(){
         var interval = setInterval(countdown, 1000);
     };
 })(jQuery);
-
-// alert和confirm美化
-// 调用方法
-// $.ztMsg.Alert('icon', 'msg', 'btntxt');
-// $.ztMsg.Confirm('icon', 'msg', 'btntxt', func);
-(function() {
-    jQuery.ztMsg = {
-        Alert: function(icon, msg, btntxt) {
-            GenerateHtml('alert', icon, msg, btntxt);
-            btnOk();
-            btnNo();
-        },
-        Confirm: function(icon, msg, btntxt, callback) {
-            GenerateHtml('confirm', icon, msg, btntxt);
-            btnOk(callback);
-            btnNo();
-        }
-    }
-    //生成Html
-    var GenerateHtml = function(type, icon, msg, btntxt) {
-        var _html = '<div class="ztpopup ztpopup-' + icon + '"><div class="ztpopup-box"><a href="javascript:;" class="ztpopup-close" id="zt-close"><span class="icon-fail"></span></a>';
-        _html += '<div class="ztpopup-ct"><span class="tipicon tipicon-' + icon + '"></span><p>' + msg + '</p></div><div class="ztpopup-btn">';
-
-        if (type == 'alert') {
-            _html += '<a class="btn" id="zt-ok" href="javascript:;">' + btntxt +'</a>';
-        }
-        if (type == 'confirm') {
-            _html += '<a class="btn-sure" id="zt-ok" href="javascript:;">' + btntxt +'</a>';
-            _html += '<a class="btn-cancel" id="zt-no" href="javascript:;">再看看</a>';
-        }
-        _html += '</div></div></div>';
-        //必须先将_html添加到body，再设置Css样式
-        jQuery('body').append(_html);
-        GenerateCss();
-    }
-
-    //生成css
-    var GenerateCss = function() {
-        var _widht = document.documentElement.clientWidth; //屏幕宽
-        var _height = document.documentElement.clientHeight; //屏幕高
-        var boxWidth = jQuery('.ztpopup-box').outerWidth();
-        var boxHeight = jQuery('.ztpopup-box').outerHeight();
-        //让提示框居中
-        jQuery('.ztpopup-box').css({
-            top: (_height - boxHeight) / 2 + 'px',
-            left: (_widht - boxWidth) / 2 + 'px'
-        });
-    }
-    //确定按钮事件
-    var btnOk = function(callback) {
-        jQuery('#zt-ok').on('click', function() {
-            jQuery('.ztpopup').remove();
-            if (typeof(callback) == 'function') {
-                callback();
-            }
-        });
-    }
-    //取消按钮事件
-    var btnNo = function() {
-        jQuery('#zt-no, #zt-close').on('click', function() {
-            jQuery('.ztpopup').remove();
-        });
-    }
-})();
