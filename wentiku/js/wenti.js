96b946c41df4f005f3e89df29891772bebd0006e
suwei-MPC <304900839@qq.com>
2019-05-21 01:54:05 +0800

    调整

--- wentiku/js/wenti.js
+++ wentiku/js/wenti.js
@@ -1,4 +1,15 @@
 $(function() {
+    // 弹窗通用
+    $('body').on('click', '.popup-box .popup-close', function () {
+        $(this).parents('.popup-box').trigger('click');
+    })
+    .on('click', '.popup-box', function () {
+        $(this).hide();
+    })
+    .on('click', '.popup-box .popup', function (e) {
+        e.stopPropagation();
+    });
+
     // 分享下拉
     $('body').on('click', '.select-btn', function(e) {
         if($(this).find('.select-list').is(':visible')) {
@@ -34,8 +45,41 @@ $(function() {
         $('.wt-so-tip-his').remove();
     });
 
+    // 删除问题
+    $('.wt-details-tit').on('click', '.wt-operate .btn-del', function () {
+        var _this = $(this);
+        $.msgBox.Confirm(null, '确定删除吗？', function () {
+            tipSave('suc', '删除成功！');
+            setTimeout(function() {
+                window.location.href = '问题后台.html';
+            }, 2000);
+        });
+    });
+
 	/** 回答按钮操作 start */
-	$('.wt-main')
+    $('.wt-main')
+    // 我来答
+    .on('click', '.btn-answer', function () {
+        if ($('.wt-answer').is(':visible')) {
+            $('.wt-answer').slideUp();
+        }
+        else {
+            $('.wt-answer').slideDown(400, function() {
+                $('body, html').animate({
+                    scrollTop: $('.wt-answer').offset().top - 117
+                }, 500);
+            });
+            textareaChange('.wt-answer', 1000);
+        }
+    })
+    // 提交答案
+    .on('click', '.wt-answer .btn-sure', function() {
+        if(!$(this).hasClass('dis')) {
+            $('.wt-answer').find('.textarea').val('');
+            $(this).addClass('dis');
+            console.log('提交成功');
+        }
+    })
     // 下拉没有帮助
 	.on('click', '.wt-operate .help', function() {
         if($(this).text() == '没有帮助') {
@@ -78,13 +122,62 @@ $(function() {
             }
             $(this).addClass('cur');
         }
-	});
-	/** 回答按钮操作 end */
+    })
+    // 展开收缩评论框
+    .on('click', '.wt-operate .btn-comment', function () {
+        var $dom = $(this).parents('.wt-details-ct').find('.wt-comment');
+        if ($dom.is(':visible')) {
+            $dom.slideUp();
+            $(this).find('.i1').show();
+            $(this).find('.i2').hide();
+        } else {
+            $dom.slideDown();
+            $(this).find('.i1').hide();
+            $(this).find('.i2').show();
+        }
+    })
+    // 删除回答
+    .on('click', '.wt-details-ct .wt-operate .btn-del', function () {
+        var _this = $(this);
+        $.msgBox.Confirm(null, '确定删除吗？', function () {
+           _this.parents('.wt-details-ct').slideUp(400, function() {
+               _this.parents('.wt-details-ct').remove();
+           });
+        });
+    })
+    // 收起回答内容
+    .on('click', '.wt-operate .btn-shou', function () {
+        $(this).parents('.wt-details-ct').addClass('wt-details-shrink');
+    })
+    // 收起回答内容
+    .on('click', '.wt-details-shrink .btn-kai', function () {
+        $(this).parents('.wt-details-ct').slideDown();
+        $(this).parents('.wt-details-ct').removeClass('wt-details-shrink');
+    });
+    /** 回答按钮操作 end */
+
+    /** 举报通用 start */
+    // 举报通用
+    $('body').on('click', '.btn-report', function () {
+        $('.popup-report').show();
+        centerObj('.popup-report .popup');
+        radioSelect('.popup-report');
+        $('.popup-report').find('label:eq(0)').trigger('click');
+        monitorVal('.popup-report .textarea', 100, 'minus');
+    })
+    .on('click', '.popup-report .btn-green', function () {
+        $('.popup-report').hide();
+        tipSave('suc', '举报成功！');
+    })
+    .on('click', '.popup-report .btn-gray', function () {
+        $('.popup-report').hide();
+        console.log('取消举报');
+    });
+    /** 举报通用 end */
 
 	/** 评论按钮操作 start */
     // 评论判断回复框字数是否输入
-	textareaChange('.wt-comment');
-
+	textareaChange('.wt-comment', 500);
 	$('.wt-comment')
 	// 回复展示更多
 	.on('click', '.more-comment', function() {
@@ -133,30 +226,57 @@ $(function() {
 	})
 	// 回复
 	.on('click', '.btn-reply', function() {
-		if($(this).parents('li').find('.reply-form').is(':visible')) {
-			$(this).parents('li').find('.reply-form').slideUp();
+		if($(this).parents('.com-box').find('.reply-form').is(':visible')) {
+			$(this).parents('.com-box').find('.reply-form').slideUp();
         }
         else {
-			$(this).parents('li').find('.reply-form').slideDown();
+            $('.wt-comment').find('.reply-form').slideUp();
+			$(this).parents('.com-box').find('.reply-form').slideDown();
         }
 	})
-	// 删除
-	.on('click', '.btn-reply', function() {
-		if($(this).parents('li').find('.reply-form').is(':visible')) {
-			$(this).parents('li').find('.reply-form').slideUp();
-        }
-        else {
-			$(this).parents('li').find('.reply-form').slideDown();
-        }
+	// 删除评论
+	.on('click', '.btn-del', function() {
+        var _this = $(this);
+        $.msgBox.Confirm(null, '确定删除吗？', function () {
+            if (_this.parents('.reply-box').length) {
+                // 删除评论子回复
+                var $numDom = _this.parents('li').find('.more-comment i');
+                if (+$numDom.html() > 1) {
+                    $numDom.html($numDom.html() - 1);
+                }
+                else {
+                    _this.parents('li').find('.more-comment, .reply-box').slideUp(400, function () {
+                        _this.parents('li').find('.more-comment, .reply-box').remove();
+                    });
+                }
+                _this.parents('.com-box').slideUp(400, function() {
+                    _this.parents('.com-box').remove();
+                });
+            }
+            else {
+                // 删除评论回复
+                _this.parents('li').slideUp(400, function () {
+                    _this.parents('li').remove();
+                });;
+            }
+        });
+	})
+	// 提交评论通用控制
+	.on('click', '.btn-sure', function () {
+	    if (!$(this).hasClass('dis')) {
+	        $('.wt-comment').find('.textarea').val('');
+	        $(this).addClass('dis');
+	        console.log('提交成功');
+	    }
 	});
 	/** 评论按钮操作 end */
 });
 
-function textareaChange(dom) {
+function textareaChange(dom, nums) {
 	$.each($(dom).find('.textarea'), function(index, val) {
 		var _this = $(this);
 		_this.unbind()
-        monitorVal(_this, 500, 'minus');
+        monitorVal(_this, nums, 'minus');
         _this.bind('input propertychange', function() {
             if (_this.val().length > 0) {
                 _this.parent().find('.btn-sure').removeClass('dis');
@@ -166,29 +286,78 @@ function textareaChange(dom) {
             }
         });
 	});
-}
+};
 
 function monitorVal(obj, nums, minus) {
     if (minus) {
-        if(jQuery(obj).nextAll('.num-box').find('.num').length) {
-            jQuery(obj).nextAll('.num-box').find('.num').html(nums - jQuery(obj).val().length);
+        if($(obj).nextAll('.num-box').find('.num').length) {
+            $(obj).nextAll('.num-box').find('.num').html(nums - $(obj).val().length);
         }
     } else {
-        if(jQuery(obj).nextAll('.num-box').find('.num').length) {
-            jQuery(obj).nextAll('.num-box').find('.num').html(jQuery(obj).val().length);
+        if($(obj).nextAll('.num-box').find('.num').length) {
+            $(obj).nextAll('.num-box').find('.num').html($(obj).val().length);
         }
     }
-
-    jQuery(obj).bind('input propertychange', function() {
-        if (jQuery(obj).val().length >= nums) {
-            jQuery(obj).val(jQuery(obj).val().substr(0, nums));
+    $(obj).unbind();
+    $(obj).bind('input propertychange', function() {
+        if ($(obj).val().length >= nums) {
+            $(obj).val($(obj).val().substr(0, nums));
         }
         if (minus) {
-            jQuery(obj).nextAll('.num-box').find('.num').html(nums - jQuery(obj).val().length);
+            $(obj).nextAll('.num-box').find('.num').html(nums - $(obj).val().length);
         } else {
-            jQuery(obj).nextAll('.num-box').find('.num').html(jQuery(obj).val().length);
+            $(obj).nextAll('.num-box').find('.num').html($(obj).val().length);
         }
     });
+};
+
+function centerObj(obj) {
+    var boxWidth = $(obj).outerWidth();
+    var boxHeight = $(obj).outerHeight();
+    $(obj).css({
+        'margin-top': -boxHeight / 2 + 'px',
+        'margin-left': -boxWidth / 2 + 'px'
+    });
+};
+
+// radio选中效果
+function radioSelect(obj) {
+    $.each($(obj).find('input[type=radio]'), function (i) {
+        if (!$(this).parents('label').find('.ico-radio').length) {
+            $(this).wrap('<span class="ico-radio"></span>');
+        }
+        if ($(this).prop('checked')) {
+            $(this).parents('span').addClass('ico-radio-cur');
+        }
+        $(this).off();
+        $(this).on('change', function () {
+            if ($(this).prop('checked')) {
+                $(obj).find('.ico-radio').removeClass('ico-radio-cur');
+                $(this).parents('span').addClass('ico-radio-cur');
+            }
+        });
+    });
+};
+
+// 保存成功失败 status为suc或者fail，cont为提示的内容
+function tipSave(status, cont, times) {
+    var time;
+    var tipTimer;
+    if (status == 'suc') {
+        icon = 'gou'
+    }
+    if (status == 'fail') {
+        icon = 'guanbi'
+    }
+    times ? time = times : time = 2000
+    $('body').append('<div class="popup-tip-box"><div class="popup-tip">' + '<span class="icon icon-' + icon + '"></span>' + '<span class="text">' + cont + '</span></div></div>');
+    $('.popup-tip').css({
+        'margin-left': -$('.popup-tip').outerWidth() / 2
+    });
+    clearInterval(tipTimer);
+    tipTimer = setTimeout(function () {
+        $('.popup-tip-box').remove();
+    }, time);
 };
 
 // alert和confirm美化
@@ -210,7 +379,7 @@ function monitorVal(obj, nums, minus) {
 	}
 	//生成Html
     var GenerateHtml = function(type, title, msg) {
-        var _html = '<div id="sw-con"><a id="sw-close" href="javascript:;"><span class="icon-close"></span></a>';
+        var _html = '<div id="sw-bg"><div id="sw-con"><a id="sw-close" href="javascript:;"><span class="icon icon-guanbi"></span></a>';
         if (title) {
             _html += '<div id="sw-tit">' + title + '</div>';
         }
@@ -223,10 +392,9 @@ function monitorVal(obj, nums, minus) {
             _html += '<a id="sw-btn-ok" href="javascript:;">确定</a>';
             _html += '<a id="sw-btn-no" href="javascript:;">取消</a>';
         }
-        _html += '</div></div>';
+        _html += '</div></div></div>';
         //必须先将_html添加到body，再设置Css样式
         jQuery('body').append(_html);
-        maskShow();
         GenerateCss();
     }
 
@@ -245,8 +413,7 @@ function monitorVal(obj, nums, minus) {
 	//确定按钮事件
     var btnOk = function(callback) {
 		jQuery('#sw-btn-ok').on('click', function() {
-			jQuery('#sw-con').remove();
-			maskHide();
+			jQuery('#sw-bg').remove();
 			if (typeof(callback) == 'function') {
 				callback();
 			}
@@ -255,8 +422,7 @@ function monitorVal(obj, nums, minus) {
 	//取消按钮事件
     var btnNo = function(callback) {
         jQuery('#sw-btn-no, #sw-close').on('click', function() {
-            jQuery('#sw-con').remove();
-            maskHide();
+            jQuery('#sw-bg').remove();
             if (typeof(callback) == 'function') {
                 callback();
             }
