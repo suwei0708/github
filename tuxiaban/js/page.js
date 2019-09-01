007f9e1e7413acfe0c82075e719b99375cb8c826
suwei-MPC <304900839@qq.com>
2019-09-02 00:29:45 +0800

    图下班

--- tuxiaban/js/page.js
+++ tuxiaban/js/page.js
@@ -1,3 +1,5 @@
+// 上传页编辑变量存储
+var editName;
 $(function() {
 	numbox();
 
@@ -41,9 +43,7 @@ $(function() {
 
 	// form通用下拉
 	$('body').on('mouseover', '.item-select dd', function() {
-		console.log(111)
 		if ($(this).find('.select-list').is(':hidden')) {
-		    console.log(222)
 			$('.select-list').hide();
 			$('.item-select').css('z-index', 'auto');
 			$(this).parents('.item-select').css('z-index', '980');
@@ -68,6 +68,40 @@ $(function() {
 			$('.select-list').hide();
 		}
 	});
+
+	// 上传页按钮是否悬浮
+	if ($('.up-form').length) {
+		var $pos = $('.up-btn-box').offset().top;
+		topbar($pos);
+		$(window).on('scroll', function() {
+			topbar($pos);
+		});
+	};
+
+	// 上传页编辑
+	$('.up-form').on('click', '.img-edit span', function() {
+		editName = $(this).parents('li');
+		$('.popup-edit').find('.up-form-box').html($(this).parents('li').html());
+		// 修改标签html
+		$('.popup-edit').find('.up-form-box').find('.item').eq(2).find('dd').html($('.popup-edit').find('.up-form-box').find('.input').eq(2))
+		$('.popup-edit').find('.up-form-box').find('.input').eq(2).attr('id', 'tagValuePopup');
+		// 初始化tag标签
+		var tagPopup = new Tag({
+		    id: 'tagValuePopup',
+		    tagtextMin: 2
+		}).initView();
+		radioSelect($('.popup-edit').find('.up-form-box .radio'))
+		$('.popup-edit').show();
+	});
+
+	// 上传页删除
+	$('body').on('click', '.popup-edit .btn-shanchu', function() {
+		var $title = $(this).parents('.up-form').find('.input').eq(0).val();
+		confirmMsg(null, '你确定要删除“ ' + $title + '”吗？<br>删除后将不可恢复 ', function() {
+			$('.popup-edit').hide();
+		    editName.remove();
+		});
+	});
 });
 
 // 字数判断
@@ -77,4 +111,13 @@ function numbox() {
             monitorVal($(this).parent().find('.input'), $(this).find('.num').text(), 'minus');
         });
     }
+}
+
+function topbar(pos) {
+	if ($(window).scrollTop() > $('.header').height() && $(window).scrollTop() < pos - $(window).height()) {
+		$('.up-btn-box').addClass('up-topbar');
+	}
+	else {
+		$('.up-btn-box').removeClass('up-topbar');
+	}
 }
